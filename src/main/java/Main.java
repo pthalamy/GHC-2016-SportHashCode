f016e5e948d1ff90d7a9d40bf8420d820de73874
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-02-09 16:48:25 +0100

    Write straightforward main to test parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import java.io.*;
+
 /**
  * Created by guillaume on 09/02/16.
  */
 public class Main {
 
-
     public static void main(String[] args){
+	
+	File inputFile = new File(args[0]);
+      
+	System.out.println("arg[0] " + args[0]);
+	
+	Picture pic = Parser.parseInput(inputFile);
 
-
+	System.out.println(pic.toString());
     }
 }
