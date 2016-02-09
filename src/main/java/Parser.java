c42a96467a798924a42f409f3347637889df25e7
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-02-09 18:38:24 +0100

    Reindent broken parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,31 +6,31 @@ public class Parser {
 
     public static Picture parseInput(File inputFile) {
 
-		try {
-			Scanner sc = new Scanner(inputFile);
-			int N = sc.nextInt();
-			int M = sc.nextInt();
+        try {
+            Scanner sc = new Scanner(inputFile);
+            int N = sc.nextInt();
+            int M = sc.nextInt();
 
-			Picture pic = new Picture(N, M);
-			String cellChar;
-			for (int R = 0; R < N; R++) {
+            Picture pic = new Picture(N, M);
+            String cellChar;
+            for (int R = 0; R < N; R++) {
                 cellChar = sc.next();
-				for (int C = 0; C < M; C++) {
+                for (int C = 0; C < M; C++) {
 
 
-					if (cellChar.charAt(C) == '#') {
+                    if (cellChar.charAt(C) == '#') {
                         pic.setCell(R, C, Color.BLACK);
                         pic.incCount();
                     }
-					else
-						pic.setCell(R, C, Color.BLANK);
-				}
-			}
+                    else
+                        pic.setCell(R, C, Color.BLANK);
+                }
+            }
 
-			return pic;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+            return pic;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
