557216b9751ba023eb7229448b501b0ae43bd831
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-02-09 20:04:00 +0100

    Fix Index Square...

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -25,18 +25,22 @@ class Square {
     }
 	
     public void paint(Picture pic) {
-        for (int i = R - S +1; i < R + S ; i++) {
-            for (int j = C - S +1; j < C + S ; j++) {
-            pic.setCell(i, j, Color.BLACK);
+        for (int i = R - S; i <= R + S ; i++) {
+            for (int j = C - S; j <= C + S ; j++) {
+				pic.setCell(i, j, Color.BLACK);
             }
         }
     }
 
     public void depaint(Picture pic) {
-        for (int i = R - S ; i < R + S - 1; i++) {
-            for (int j = C - S ; j < C + S - 1; j++) {
+        for (int i = R - S ; i <= R + S; i++) {
+            for (int j = C - S ; j <= C + S; j++) {
                 pic.setCell(i, j, Color.depaint(pic, pic.getCellValue(i,j),false));
             }
         }
     }
+
+	public String toString() {
+		return "R " + this.R + " C " + this.C + " S " + this.S;
+	}
 }
