d5daa1dbabbd441b02b09db0fa5dfb527dea791b
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-02-09 17:02:29 +0100

    Args check and output file name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,21 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args){
+
+	if (args.length < 1) {
+	    System.err.println("error: missing parameter: input file");
+	    System.err.println("usage: launch.sh input.in");
+	    System.exit(1);
+	}
+
+	String inputFileName = args[0];
+	int extIndex = inputFileName.lastIndexOf(".in");
+        String outputFileName = inputFileName.substring(0, extIndex) + ".out";
 	
-	File inputFile = new File(args[0]);
-      
-	System.out.println("arg[0] " + args[0]);
+	File inputFile = new File(inputFileName);
+	File outputFile = new File(outputFileName);
 	
-	Picture pic = Parser.parseInput(inputFile);
+	Picture pic = Parser.parseInput(inputFile);	
 
 	System.out.println(pic.toString());
     }
