8104b7d4c54591ab84b1e32e8b806d8767ba7876
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-02-11 16:54:08 +0100

    Main Skeleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,33 @@
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println("test");
+        if (args.length < 1) {
+            System.err.println("error: missing parameter: input file");
+            System.err.println("usage: launch.sh input.in");
+            System.exit(1);
+        }
+
+		String inputFileName = args[0];
+		
+		System.out.println("Output File Name: " + getOutputFileName(inputFileName));
+
+		// Parser.parse(inputFileName);
+
+		
 	}
 
+
+	public static String getOutputFileName(String inputFileName) {
+		int extIndex = inputFileName.lastIndexOf(".in");
+
+		if (extIndex == -1) {
+			System.err.println("error: invalid input file extension: expected .in");
+            System.err.println("usage: launch.sh input.in");
+			System.exit(1);
+		}
+
+		String outputFileName = inputFileName.substring(0, extIndex) + ".out";
+
+		return outputFileName;
+	}
 }
