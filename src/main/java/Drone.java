6182cac09be14cc2bfb33671d8ba201bdbb1d03b
Erwan Mathonière <erwan.mathoniere@ensimag.grenoble-inp.fr>
2016-02-11 19:39:39 +0100

    Rajout structure Drone

--- src/main/java/Drone.java
+++ src/main/java/Drone.java
@@ -22,4 +22,40 @@ public class Drone {
                 ", commands=" + commands +
                 '}';
     }
+    
+    public void update(Data data) {
+        switch (this.state) {
+            case INIT:
+                init(data);
+                break;
+                
+            case DELIVERING:
+                delivering(data);
+                break;
+               
+            case GOINGBACK:
+                goingback(data);
+                break;
+                
+            case LOADING:
+                loading(data);
+                break;
+        }
+    }
+
+    private void init(Data data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void delivering(Data data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void goingback(Data data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void loading(Data data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
