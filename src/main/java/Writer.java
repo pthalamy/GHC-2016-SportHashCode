e6cd460b294a8d3d679e7ce82a23315723fff6ad
Guillaume Mallet <guillaume.mallet@ensimag.grenoble-inp.fr>
2016-02-11 17:27:45 +0100

    synchronized

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -29,47 +29,46 @@ public class Writer {
         }
     }
 
-    public static void add(Line l){
+    public synchronized static void add(Line l){
         count++;
         builder.add(l);
     }
 
-    public static Line add(String s){
+    public synchronized static Line add(String s){
         count++;
         Line l = new Line(s);
         builder.add(l);
         return l;
     }
 
-    public static Line add(Object o){
+    public synchronized static Line add(Object o){
         count++;
         return add(o.toString());
     }
 
-    public static Line add(String s,int index){
+    public synchronized static Line add(String s,int index){
         count++;
         Line l = new Line(s);
         builder.add(index,l);
         return l;
     }
 
-    public static void add(Line l,int index){
+    public synchronized static void add(Line l,int index){
         count++;
         builder.add(index,l);
     }
 
-    public static Line add(Object o,int index){
+    public synchronized static Line add(Object o,int index){
         count++;
         return add(o.toString(),index);
     }
 
-    public static void display(){
+    public synchronized static void display(){
         System.out.println(count);
         builder.forEach(System.out::println);
     }
 
     public static void main(String[] args){
-
         display();
         writeInFile("test.t");
     }
