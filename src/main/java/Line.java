7bf84a6577d276a222a4de2e7507079891fbe16d
Guillaume Mallet <guillaume.mallet@ensimag.grenoble-inp.fr>
2016-02-11 17:07:10 +0100

    complete writer

---
+++ src/main/java/Line.java
@@ -0,0 +1,15 @@
+/**
+ * Created by guillaume on 11/02/16.
+ */
+public class Line {
+    public String s;
+
+    public Line(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+}
---
+++ src/main/java/Writer.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by guillaume on 09/02/16.
+ */
+public class Writer {
+    private static List<Line> builder = new LinkedList<>();
+    private static int count = 0;
+
+    public static void writeInFile(String file){
+        try {
+            File f = new File(file);
+            FileWriter out = new FileWriter(f);
+            builder.forEach(s -> {
+                try {
+                    out.write(s.toString());
+                    out.write("\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            out.flush();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void add(Line l){
+        count++;
+        builder.add(l);
+    }
+
+    public Line add(String s){
+        count++;
+        Line l = new Line(s);
+        builder.add(l);
+        return l;
+    }
+
+    public Line add(Object o){
+        count++;
+        return add(o.toString());
+    }
+
+    public Line add(String s,int index){
+        count++;
+        Line l = new Line(s);
+        builder.add(index,l);
+        return l;
+    }
+
+    public void add(Line l,int index){
+        count++;
+        builder.add(index,l);
+    }
+
+    public Line add(Object o,int index){
+        count++;
+        return add(o.toString(),index);
+    }
+
+    public static void display(){
+        System.out.println(count);
+        System.out.println(builder.toString());
+    }
+
+    public static void main(String[] args){
+        display();
+        writeInFile("test.t");
+    }
+}
